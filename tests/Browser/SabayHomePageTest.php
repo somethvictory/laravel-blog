9037ca769381b2ff25877f7257f9fc2366edc5ac
Someth Victory <victory.someth@gmail.com>
2017-08-25 08:59:23 +0700

    add more spec on sabay.com.kh

--- tests/Browser/SabayHomePageTest.php
+++ tests/Browser/SabayHomePageTest.php
@@ -13,15 +13,25 @@ class SabayHomePageTest extends DuskTestCase
     //A basic browser test example.
 
     //@return void
-  public function testRequiredElements()
-  {
+  public function testNavigation() {
     $this->browse(function (Browser $browser) {
       $browser->visit('http://news.sabay.com.kh')
         ->assertVisible('li#entertainment')
         ->assertVisible('li#technology')
         ->assertVisible('li#life')
-        ->assertVisible('li#sport');
+        ->assertVisible('li#sport')
+        ->assertVisible('a[href="http://kanha.sabay.com.kh"]')
+        ->assertVisible('a[href="http://enovel.sabay.com.kh"]')
+        ->assertVisible('a[href="http://der.sabay.com.kh"]');
     });
-    
+
+  }
+
+  public function testSidebarAds() {
+    $this->browse(function(Browser $browser) {
+    $browser->visit('http://news.sabay.com.kh')
+      ->assertVisible('div#ad_zone_02')
+      ->assertVisible('div#ad_zone_03');
+    });
   }
 }
